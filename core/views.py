28cd3717bcef5a3f3c1775ff2f175c36ccbdf2e6
Eiad Soufan <eiad.soufan.2@gmail.com>
2026-01-10 00:55:11 +0800

    Update views.py

--- core/views.py
+++ core/views.py
@@ -4,6 +4,41 @@ from .serializers import *
 from .pagination import StandardResultsSetPagination
 
 
+import json
+import subprocess
+import tempfile
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
+from django.views.decorators.http import require_GET
+@require_GET
+def export_database(request):
+    with tempfile.NamedTemporaryFile(suffix=".json", delete=False) as tmp:
+        tmp_path = tmp.name
+
+    try:
+        subprocess.check_call([
+            "python",
+            "manage.py",
+            "dumpdata",
+            "--natural-foreign",
+            "--natural-primary",
+            "-e", "contenttypes",
+            "-e", "auth.Permission",
+            "--indent", "2",
+        ], stdout=open(tmp_path, "w"))
+
+        with open(tmp_path, "rb") as f:
+            response = HttpResponse(
+                f.read(),
+                content_type="application/json"
+            )
+            response["Content-Disposition"] = 'attachment; filename="database_dump.json"'
+            return response
+
+    finally:
+        if os.path.exists(tmp_path):
+            os.remove(tmp_path)
 
 
 
@@ -84,4 +119,5 @@ class VideoListAPIView(ListAPIView):
     serializer_class = VideoSerializer
 
     def get_queryset(self):
+
         return Video.objects.filter(status="published").order_by("id")
